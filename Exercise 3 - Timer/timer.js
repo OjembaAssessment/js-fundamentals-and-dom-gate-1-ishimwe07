66604816b35f04a93b9e0774507bda1b6fcb47b8
Ishimwe <ishimwgabby@gmail.com>
2023-07-24 09:50:35 +0200

    Added index.html

--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -1,1 +1,14 @@
-console.log("Exercise 3 - Timer");
+const stopBtn = document.querySelector("#stop");
+const startBtn = document.querySelector("#start");
+const forms = document.querySelector(".forms");
+const timeRem = document.querySelector(".tm");
+
+
+function startTimer(){
+    startBtn.style.display = "none";
+    stopBtn.styles.display = "block";
+    forms.styles.display = "none";
+    timeRem.styles.display = "block"
+}
+
+startBtn.addEventListener("click", startTimer())
