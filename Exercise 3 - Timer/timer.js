6b11e80487eef84265a2718d9a7318f8d448b587
Ishimwe <ishimwgabby@gmail.com>
2023-07-24 11:09:32 +0200

    Added Question 3

--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -1,14 +1,48 @@
 const stopBtn = document.querySelector("#stop");
-const startBtn = document.querySelector("#start");
+const startBtn = document.querySelector("#Start");
+const resetBtn = document.querySelector("#reset");
 const forms = document.querySelector(".forms");
 const timeRem = document.querySelector(".tm");
+const seconds = document.querySelector("#seconds");
+const secs = document.querySelector("#sec");
+const minusSign = document.querySelector(".minus");
+const plusSign = document.querySelector(".plus");
 
 
-function startTimer(){
-   // startBtn.style.display = "none";
-    stopBtn.styles.display = "block";
-    forms.styles.display = "none";
-    timeRem.styles.display = "block"
+function timers(){
+    if(seconds.value>=0){
+        secs.textContent = seconds.value--;
+    }
+
 }
+startBtn.addEventListener("click", function StartTiming(){
+    startBtn.style.display = "none"
+    forms.style.display = "none";
+    timeRem.style.display = "block";
+    stopBtn.style.display = "block";
+    setInterval(() => timers()
+        , 1000);
+});
 
-startBtn.addEventListener("click", startTimer())
+resetBtn.addEventListener("click", function(){
+    location.reload();
+});
+
+stopBtn.addEventListener("click", function(){
+    clearInterval(timers);
+});
+
+minusSign.addEventListener("click", function(){
+    setTimeout(() => {
+        seconds.value = +seconds.value - 1;
+        secs.textContent = seconds.value;
+    }, 1);
+    
+})
+plusSign.addEventListener("click", function(){
+    setTimeout(() => {
+        seconds.value = +seconds.value + 1;
+        secs.textContent = seconds.value;
+    }, 1);
+})
+
